47144965fce926cb351d38ebce43854251bd481c
muhamad <muhamad@future.rent>
2019-11-30 05:14:25 +0100

    remove console.log and add key props

--- src/components/lists.tsx
+++ src/components/lists.tsx
@@ -36,7 +36,6 @@ const Button = styled.button`
 const Lists: React.FC<{ data: any }> = ({ data }) => {
   const renderList = () => {
     return data.map((post: any) => {
-      console.log(post);
       return (
         <Images key={post.id}>
           <User>
@@ -51,7 +50,7 @@ const Lists: React.FC<{ data: any }> = ({ data }) => {
               showStatus={false}
             >
               {post.postsURL.map((photo: string, i: number) => {
-                return <img src={photo} alt={i.toString()} />;
+                return <img src={photo} key={i} alt={i.toString()} />;
               })}
             </Carousel>
           ) : (
