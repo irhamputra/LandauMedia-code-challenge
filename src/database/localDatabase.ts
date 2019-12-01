508d29f58634f31920ff7dbe0bc37b0aec1a4c59
muhamad <muhamad@future.rent>
2019-12-01 07:21:57 +0100

    init db

--- src/database/localDatabase.ts
+++ src/database/localDatabase.ts
@@ -5,39 +5,8 @@ import { DATA, USER } from "./mockdata";
 const adapter = new LocalStorage("db");
 export const db = low(adapter);
 
-// init new post
 export const initDB = () => {
   console.log("init DB");
   db.defaults({ postsCollection: DATA, userCollection: USER }).write();
   db.defaults({ loggedIn: {} }).write();
-};
-
-export const postPhoto = (data: object) => {
-  return (
-    db
-      .get("posts")
-      // @ts-ignore
-      .push(data)
-      .write()
-  );
-};
-
-export const postComment = (data: object) => {
-  return (
-    db
-      .get("comments")
-      // @ts-ignore
-      .push(data)
-      .write()
-  );
-};
-
-export const userRegister = (data: object) => {
-  return async () => {
-    await db
-      .get("postsCollection")
-      // @ts-ignore
-      .push(data)
-      .write();
-  };
 };
